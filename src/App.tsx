1b1bbeb190e49c090b99598860418bd05c75f1e8
hrsk <hrsk.yegor@gmail.com>
2025-01-06 17:45:59 +0300

    useMemo, searching posts on title or text post

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './styles/App.css'
 import {PostType} from "./components/PostItem.tsx";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import {PostList} from './components/PostList.tsx';
 import {PostForm} from "./components/PostForm.tsx";
 import {CustomSelect, OptionValue, SortKeyType} from "./components/UI/select/CustomSelect.tsx";
+import {CustomInput} from "./components/UI/input/CustomInput.tsx";
 
 function App() {
 
@@ -18,11 +19,28 @@ function App() {
     ])
 
     const [selectedSort, setSelectedSort] = useState<SortKeyType | ''>('')
+    const [searchQuery, setSearchQuery] = useState<string>('')
 
     const optionValues: OptionValue[] = [
         {optionValue: 'title', optionTitle: 'Sort by title'},
         {optionValue: 'postText', optionTitle: 'Sort by text'},
     ]
+
+    const sortedPosts = useMemo(() => {
+        console.log('get sorted post')
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts
+    }, [selectedSort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        if (selectedSort === 'title') {
+            return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+        } else {
+            return sortedPosts.filter(post => post.postText.toLowerCase().includes(searchQuery.toLowerCase()))
+        }
+    }, [selectedSort, searchQuery, sortedPosts])
 
     const addPost = (newPost: PostType) => {
         setPosts([...posts, newPost])
@@ -34,20 +52,25 @@ function App() {
 
     const handleSortChange = (sortKey: SortKeyType) => {
         setSelectedSort(sortKey)
-        setPosts([...posts].sort((a, b) => a[sortKey].localeCompare(b[sortKey])))
+        // setPosts([...posts].sort((a, b) => a[sortKey].localeCompare(b[sortKey])))
     }
 
     return (
         <div className={"App"}>
             <PostForm addPost={addPost}/>
             <div style={{padding: '15px 0'}}/>
+            <CustomInput value={searchQuery}
+                         onChange={(e) => setSearchQuery(e.currentTarget.value)}
+                         placeholder={'Search...'}/>
             <CustomSelect options={optionValues}
                           selectedSort={selectedSort}
                           defaultValue={'Sort by'}
                           onChange={handleSortChange}/>
             {
-                posts.length
-                    ? <PostList posts={posts} removePost={removePost} title={'Список постов про Javascript'}/>
+                sortedAndSearchedPosts.length
+                    // ? <PostList posts={posts} removePost={removePost} title={'Список постов про Javascript'}/>
+                    ? <PostList posts={sortedAndSearchedPosts} removePost={removePost}
+                                title={'Список постов про Javascript'}/>
                     : <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
             }
 
